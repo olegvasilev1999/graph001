c89b4946e66b8414d50a86095a4436a99464162c
olegvasilev1999 <32066946+olegvasilev1999@users.noreply.github.com>
2018-05-14 22:46:09 +0600

    Create graph.cpp

---
+++ include/graph.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+class graph
+{
+	int ** mtrx;
+	int vertices;
+	int * visiting;
+
+public:
+	graph() {
+		mtrx = nullptr;
+		vertices = 0;
+		visiting = nullptr;
+	}
+
+	graph(int n) {
+		vertices = n;
+		mtrx = new int *[vertices];
+		visiting = new int [vertices];
+		for (int i = 0; i < vertices; i++) {
+			mtrx[i] = new int[vertices];
+			visiting[i] = 0;
+		}
+	}
+
+	bool read(std::istream & stream)
+	{
+		int n;
+		bool success = true;
+		if (stream >> n) {
+			int ** elements = new int *[n];
+			for (int i = 0; success && i < n; i++) {
+				elements[i] = new int[n];
+				for (int j = 0; j < n; ++j) {
+					if (!(stream >> elements[i][j])) {
+						success = false;
+						break;
+					}
+				}
+			}
+			if (success) {
+				vertices = n;
+				mtrx = elements;
+				visiting = new int[vertices];
+				for (int i = 0; i < vertices; i++) {
+					visiting[i] = 0;
+				}
+			}
+			else {
+				for (int i = 0; i < n; i++) {
+					delete[] elements[i];
+				}
+				delete[] elements;
+			}
+		}
+		else {
+			success = false;
+		}
+		return success;
+	}
+
+	void graph_dfs(std::ostream & stream, int vertex) {
+		if (vertex < vertices) {
+			visiting[vertex]++;
+			stream << vertex << " ";
+			for (int i = 0; i < vertices; i++) {
+				if (mtrx[vertex][i] == 1 && visiting[i] == 0) {
+					graph_dfs(stream, i);
+				}
+			}
+		}
+		else {
+			throw out_of_range("there is no such vertex");
+		}
+	}
+
+
+	~graph(){
+		if (mtrx != nullptr) {
+			for (int i = 0; i < vertices; i++)
+			{
+				delete[] mtrx[i];
+			}
+			delete[] mtrx;
+			delete[] visiting;
+		}
+	}
+};
