c97337a42ef44be0960e79184e3dbdae7f529a8d
olegvasilev1999 <32066946+olegvasilev1999@users.noreply.github.com>
2018-05-14 23:14:32 +0600

    Update graphtest.cpp

--- tests/graphtest.cpp
+++ tests/graphtest.cpp
@@ -1,7 +1,36 @@
 #include <catch.hpp>
 #include "graph.cpp"
 
-
-TEST_CASE("insert and remove")
+TEST_CASE("graph1")
 {
+  string input {
+     "1 1 1 1 1\n"
+     "1 1 1 1 1\n"
+     "1 1 1 1 1\n"
+     "1 1 1 1 1\n"
+     "1 1 1 1 1\n"
+    };
+  
+  string result = "1 0 2 3 4 ";
+  
+  graph Graph(5);
+  Graph.read_mtrx(input);
+  
+  std::ostringstream ostream;
+  Graph.graph_dfs(ostream, 1);
+  
+  REQUIRE(ostream == result);
+}
+
+TEST_CASE("wrong size")
+{
+  string input {
+     "1 1 1\n"
+     "1 1 1\n"
+     "1 1 1\n"
+    };
+  graph Graph(3);
+  Graph.read_mtrx(input);
+  
+  REQUIRE_THROWS(Graph.graph_dfs(ostream, 5););
 }
