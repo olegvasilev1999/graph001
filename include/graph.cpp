a0c242019c26914582840d48a0c8e40fd4b61307
olegvasilev1999 <32066946+olegvasilev1999@users.noreply.github.com>
2018-06-15 19:18:56 +0300

    Update graph.cpp

--- include/graph.cpp
+++ include/graph.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <sstream>
 #include <stdexcept>
 
@@ -20,7 +21,7 @@ public:
 	graph(int n) {
 		vertices = n;
 		mtrx = new int *[vertices];
-		visiting = new int [vertices];
+		visiting = new int[vertices];
 		for (int i = 0; i < vertices; i++) {
 			mtrx[i] = new int[vertices];
 			visiting[i] = 0;
@@ -42,13 +43,13 @@ public:
 				}
 			}
 			if (success) {
-				if (mtrx != nullptr){
+				if (mtrx != nullptr) {
 					for (int i = 0; i < vertices; i++) {
-					delete[] mtrx[i];
+						delete[] mtrx[i];
+					}
+					delete[] mtrx;
 				}
-				delete[] mtrx;
-				}
-				mtrx = new int * [vertices];
+				mtrx = new int *[vertices];
 				for (int i = 0; i < vertices; i++)
 				{
 					mtrx[i] = new int[vertices];
@@ -61,10 +62,11 @@ public:
 					delete[] elements[i];
 				}
 				delete[] elements;
-			}else {
+			}
+			else {
 				success = false;
 			}
-			
+
 		}
 		else {
 			success = false;
@@ -87,15 +89,80 @@ public:
 		}
 	}
 
+	void clearvisiting() {
+		for (int i = 0; i < vertices; i++) {
+			visiting[i] = 0;
+		}
+	}
 
-	~graph(){
+	int * distance(int vertex) {
+		int * dist = new int[vertices];
+		int minindex, min;
+		clearvisiting();
+		for (int i = 0; i < vertices; i++) {
+			dist[i] = 10000;
+		}
+		dist[vertex] = 0;
+		do {
+			minindex = 10000;
+			min = 10000;
+			for (int i = 0; i < vertices; i++) {
+				if (dist[i] < min && visiting[i] == 0 ) {
+					min = dist[i];
+					minindex = i;
+				}
+			}
+			if (minindex != 10000) {
+				for (int i = 0; i < vertices; i++) {
+					if (mtrx[minindex][i] > 0) {
+						if (min + mtrx[minindex][i] < dist[i]) {
+							dist[i] = min + mtrx[minindex][i];
+						}
+					}
+				}
+				visiting[minindex] = 1;
+			}
+		} while (minindex < 10000);
+		return dist;
+	}
+
+	void shortway(std::ostream & stream, int vertex) {
+		int * d = distance(vertex);
+		int j, dist, tmp;
+		for (int dest = 0; dest < vertices; dest++){
+			int * way = new int[vertices];
+			way[0] = dest;
+			tmp = dest;
+			j = 1;
+			dist = d[dest];
+			while (tmp != vertex) {
+				for (int i = 0; i < vertices; i++) {
+					if (mtrx[tmp][i] != 0) {
+						if (dist - mtrx[tmp][i] == d[i]) {
+							dist = dist - mtrx[tmp][i];
+							tmp = i;
+							way[j] = i;
+							j++;
+						}
+					}
+				}
+			}
+			for (int i = j - 1; i >= 0; i--) {
+				stream << way[i] << ' ';
+			}
+			stream << endl;
+			delete[] way;
+		}
+		delete[] d;
+		
+	}
+
+	~graph() {
 		if (mtrx != nullptr) {
-			for (int i = 0; i < vertices; i++)
-			{
+			for (int i = 0; i < vertices; i++) {
 				delete[] mtrx[i];
 			}
 			delete[] mtrx;
 			delete[] visiting;
 		}
 	}
-};
